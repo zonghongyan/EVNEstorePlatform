8eb4352aa37d385bef43e081a97a19e87e7adb4d
zonghongyan <renboan@aliyun.com>
2016-12-30 13:45:25 +0800

    add file comment

---
+++ EVNEstorePlatform/EVNEstorePlatform/Classes/Center/Controllers/EVNTabBarController.m
@@ -0,0 +1,28 @@
+//
+//  EVNTabBarController.m
+//  EVNEstorePlatform
+//
+//  Created by developer on 2016/12/30.
+//  Copyright © 2016年 仁伯安. All rights reserved.
+//
+
+#import "EVNTabBarController.h"
+
+@interface EVNTabBarController ()
+
+@end
+
+@implementation EVNTabBarController
+
+- (void)viewDidLoad
+{
+    [super viewDidLoad];
+    // Do any additional setup after loading the view.
+}
+
+- (void)didReceiveMemoryWarning {
+    [super didReceiveMemoryWarning];
+    // Dispose of any resources that can be recreated.
+}
+
+@end
